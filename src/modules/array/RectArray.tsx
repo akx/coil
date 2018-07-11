44779f72f22e9a0e015b5fac06ee3b869ff3bdf6
Aarni Koskela <akx@iki.fi>
2018-07-11 16:40:45 +0300

    Teach RectArray some useful hexes
    H/T https://www.redblobgames.com/grids/hexagons/ for the math

--- src/modules/array/RectArray.tsx
+++ src/modules/array/RectArray.tsx
@@ -3,6 +3,10 @@ import Module from '../Module';
 import Context from '../../Context';
 import {renderNodesInto} from '../../render';
 
+const THREE_OVER_TWO = (3 / 2);
+const SQRT_THREE = Math.sqrt(3);
+const SQRT_THREE_OVER_TWO = SQRT_THREE / 2;
+
 export default {
   acceptsChildren: true,
   variables: [
@@ -10,24 +14,41 @@ export default {
     {name: 'numberY', default: '5'},
     {name: 'variableX', default: 'vx'},
     {name: 'variableY', default: 'vy'},
+    {
+      name: 'hex',
+      default: 'none',
+      choices: [
+        'none',
+        'flat',
+        'pointy',
+      ],
+    }
   ],
 
   render(context: Context) {
     const {node} = context;
     // tslint:disable-next-line:prefer-const
-    let {numberX, numberY, variableX, variableY} = context.evaluateNodeConfig(node);
+    let {numberX, numberY, variableX, variableY, hex} = context.evaluateNodeConfig(node);
     const nodes = [];
 
     numberX = Math.round(parseFloat(numberX));
     numberY = Math.round(parseFloat(numberY));
     for (let y = 0; y < numberY; y++) {
       for (let x = 0; x < numberX; x++) {
-        const subcontext = context.subcontext(node, {
+        const newVariables = {
           [variableX]: x,
           [variableY]: y,
           [`${variableX}F`]: x / (numberX - 1),
           [`${variableY}F`]: y / (numberY - 1),
-        }, `${x}x${y}`);
+        };
+        if (hex === 'flat') {
+          newVariables[`${variableX}Hex`] = THREE_OVER_TWO * x;
+          newVariables[`${variableY}Hex`] = SQRT_THREE_OVER_TWO * x + SQRT_THREE * y;
+        } else if (hex === 'pointy') {
+          newVariables[`${variableX}Hex`] = SQRT_THREE * x + SQRT_THREE_OVER_TWO * y;
+          newVariables[`${variableY}Hex`] = THREE_OVER_TWO * y;
+        }
+        const subcontext = context.subcontext(node, newVariables, `${x}x${y}`);
         renderNodesInto(nodes, node.children, subcontext);
       }
     }
