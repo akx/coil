133c276fc2fd4c471cde9776184f79efdd4ba53a
Aarni Koskela <akx@iki.fi>
2020-04-29 07:02:31 -0300

    Updates

---
+++ precache-manifest.8fb98ee64070753ab006671cc3396c81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "015094aaad5e29185f9774c0069ba040",
+    "url": "/coil/index.html"
+  },
+  {
+    "revision": "a11732fdb54fbea16606",
+    "url": "/coil/static/css/main.345ec689.chunk.css"
+  },
+  {
+    "revision": "a5376c26828ded416840",
+    "url": "/coil/static/js/2.b349efe3.chunk.js"
+  },
+  {
+    "revision": "d9fe8be9ef7b2c5a03aa3824c458a393",
+    "url": "/coil/static/js/2.b349efe3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a11732fdb54fbea16606",
+    "url": "/coil/static/js/main.c8f67172.chunk.js"
+  },
+  {
+    "revision": "504a6a268831e274956f",
+    "url": "/coil/static/js/runtime-main.302c0688.js"
+  }
+]);
